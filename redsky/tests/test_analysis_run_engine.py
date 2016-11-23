37779a57db53fef53da7a507281560d82b9b9fc5
christopher <cjwright4242@gmail.com>
2016-11-22 21:59:31 -0500

    STY: rename some things to conform to PEP8

--- redsky/tests/test_analysis_run_engine.py
+++ redsky/tests/test_analysis_run_engine.py
@@ -11,13 +11,12 @@ def example_run_func(event_stream1, event_stream2, name):
 
 
 def example_fail_run_func(event_stream1, event_stream2):
-    for e1, e2 in zip(event_stream1, event_stream2):
-        yield NotImplementedError('Known Error')
+    yield NotImplementedError('Known Error')
 
 
 def example_run_func2(event_stream1, name):
     for e1 in event_stream1:
-        yield 2*e1['data'][name]
+        yield 2 * e1['data'][name]
 
 
 run_functions = [(example_run_func, 'pe1_image'),
@@ -26,14 +25,14 @@ run_functions = [(example_run_func, 'pe1_image'),
 
 @pytest.mark.parametrize("run_and_args", run_functions)
 def test_analysis_run_engine(exp_db, tmp_dir, run_and_args):
-    RF = RunFunction(run_and_args[0], ['img'],
-                           [dict(source='testing',
-                                 external='FILESTORE:',
-                                 dtype='array')], save_func=np.save,
-                           save_loc=tmp_dir)
+    r_f = RunFunction(run_and_args[0], ['img'],
+                      [dict(source='testing',
+                            external='FILESTORE:',
+                            dtype='array')], save_func=np.save,
+                      save_loc=tmp_dir)
     are = AnalysisRunEngine(exp_db)
     run_hdrs = exp_db[-1]
-    uid = are(run_hdrs, RF, run_and_args[1])
+    uid = are(run_hdrs, r_f, run_and_args[1])
     pprint(exp_db[uid])
     assert exp_db[uid]['stop']['exit_status'] != 'failure'
     assert len(list(exp_db.get_events(exp_db[uid]))) == len(list(
