b0d3a62ddefbea7d68988ea74d59aee95bb8f9c6
christopher <cjwright4242@gmail.com>
2016-11-22 21:06:59 -0500

    TST: add test of failure

--- redsky/tests/test_analysis_run_engine.py
+++ redsky/tests/test_analysis_run_engine.py
@@ -9,6 +9,11 @@ def example_run_func(event_stream1, event_stream2, name):
         yield e1['data'][name] - e2['data'][name]
 
 
+def example_fail_run_func(event_stream1, event_stream2):
+    for e1, e2 in zip(event_stream1, event_stream2):
+        yield NotImplementedError('Known Error')
+
+
 def test_analysis_run_engine(exp_db, tmp_dir):
     subtract = RunFunction(example_run_func, ['img'],
                            [dict(source='testing',
@@ -21,4 +26,19 @@ def test_analysis_run_engine(exp_db, tmp_dir):
     pprint(exp_db[uid])
     assert exp_db[uid]['stop']['exit_status'] != 'failure'
     assert len(list(exp_db.get_events(exp_db[uid]))) == len(list(
+        exp_db.get_events(run_hdrs)))
+
+
+def test_analysis_run_engine_fail(exp_db, tmp_dir):
+    subtract = RunFunction(example_run_func, ['img'],
+                           [dict(source='testing',
+                                 external='FILESTORE:',
+                                 dtype='array')], save_func=np.save,
+                           save_loc=tmp_dir)
+    are = AnalysisRunEngine(exp_db)
+    run_hdrs = exp_db[-1]
+    uid = are([exp_db[-1], exp_db[-1]], subtract)
+    pprint(exp_db[uid])
+    assert exp_db[uid]['stop']['exit_status'] == 'failure'
+    assert len(list(exp_db.get_events(exp_db[uid]))) != len(list(
         exp_db.get_events(run_hdrs)))
