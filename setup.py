bd4f428d0fdff7e9627a3acf1415391e791d5a87
christopher <cjwright4242@gmail.com>
2016-11-29 21:11:18 -0500

    DEV: add the usual dev stack

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sunset',
+    name='redsky',
     version='0.0.0-alpha',
     packages=find_packages(),
     # description='data processing module',
     zip_safe=False,
-    url='http:/github.com/xpdAcq/sunset'
+    url='http:/github.com/xpdAcq/redsky'
 )
