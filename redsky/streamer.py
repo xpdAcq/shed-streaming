ed13562ecc53ddb76098701364cc9da1937620b0
christopher <cjwright4242@gmail.com>
2016-12-03 21:07:48 -0500

    STY: remove print statements

--- redsky/streamer.py
+++ redsky/streamer.py
@@ -44,9 +44,7 @@ def db_store(db, fs_data_name_save_map=None):
                                            sub_dict['datum_kwargs'])
 
                         # 3. Replace the array in the fs_doc with the uid
-                        print('before', doc['data'][data_name])
                         fs_doc['data'][data_name] = fs_uid
-                        print('after', doc['data'][data_name])
                     doc.update(
                         filled={k: True for k in fs_data_name_save_map.keys()})
                 # Always stash the (potentially) filestore mutated doc
@@ -57,39 +55,3 @@ def db_store(db, fs_data_name_save_map=None):
         return wrapped_f
 
     return wrap
-
-
-"""
-    def sample_f(name_doc_stream_pair, **kwargs):
-        process = multiply_by_two
-        _, start = next(name_doc_stream_pair)
-        new_start_doc = {'parents': start['uid'],
-                         'function_name': process.__name__,
-                         'kwargs': kwargs}  # More provenance to be defined
-        yield 'start', new_start_doc
-        _, descriptor = next(name_doc_stream_pair)
-        new_descriptor = dict(data_keys={'img': dict(source='testing')})
-        yield 'descriptor', new_descriptor
-        exit_md = None
-        for i, (name, ev) in enumerate(name_doc_stream_pair):
-            if name == 'stop':
-                break
-            args_mapping = [ev['data'][k] for k in ['pe1_image']]
-            kwargs_mapping = {}
-            kwargs_mapped = {k: ev[v] for k, v in kwargs_mapping.items()}
-            # try:
-            results = process(*args_mapping, **kwargs_mapped,
-                                  **kwargs)
-            # except Exception as e:
-            # exit_md = dict(exit_status='failure', reason=repr(e),
-            #                    traceback=traceback.format_exc())
-            new_event = dict(descriptor=new_descriptor,
-                             data={'img': results},
-                             seq_num=i)
-            yield 'event', new_event
-        if name == 'stop':
-            if exit_md is None:
-                exit_md = {'exit_status': 'success'}
-            new_stop = dict(** exit_md)
-            yield 'stop', new_stop
-"""
