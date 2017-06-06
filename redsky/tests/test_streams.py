c5d2834c4c92c8000a7976505e1b42123abe6d2d
christopher <cjwright4242@gmail.com>
2017-06-05 21:55:17 -0400

    WIP: update the design manifesto

--- redsky/tests/test_streams.py
+++ redsky/tests/test_streams.py
@@ -79,9 +79,31 @@ def test_combine_latest(exp_db, start_uid1, start_uid3):
     ih2 = exp_db[start_uid3]
     s = exp_db.restream(ih1, fill=True)
     s2 = exp_db.restream(ih2, fill=True)
+    d = {'source1': {'source': source, 'gen': s, 'finished': False},
+         'source2': {'source': source2, 'gen': s2, 'finished': False}}
+    while not all([z['finished'] for z in d.values()]):
+        for k in ['source1', 'source2']:
+            v = d[k]
+            if not v['finished']:
+                try:
+                    v['source'].emit(next(v['gen']))
+                except StopIteration:
+                    v['finished'] = True
+    for l in L:
+        print(list(zip(*l))[0])
+
+
+def test_zip(exp_db, start_uid1, start_uid2):
+    source = Stream()
+    source2 = Stream()
+
+    L = source.zip(source2).sink_to_list()
+    ih1 = exp_db[start_uid1]
+    ih2 = exp_db[start_uid2]
+    s = exp_db.restream(ih1, fill=True)
+    s2 = exp_db.restream(ih2, fill=True)
     for a in s:
         source.emit(a)
-    for a in s2:
         source2.emit(a)
     for l in L:
         print(list(zip(*l))[0])
