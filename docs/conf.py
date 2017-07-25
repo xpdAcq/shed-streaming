9d33992776d828f4c022b1453fb44567d41d1395
christopher <cjwright4242@gmail.com>
2017-07-25 16:46:57 -0400

    DOC: fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,16 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode']
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,7 +92,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
