9508057c6362a2c05680470c9a8f1a9482573f55
christopher <cjwright4242@gmail.com>
2017-08-02 13:24:56 -0400

    WIP: build demo example for xpd

---
+++ examples/xpd_alpha_workflow.py
@@ -0,0 +1,231 @@
+"""Example for XPD data"""
+from itertools import islice
+from pprint import pprint
+
+import numpy as np
+from bluesky.callbacks.broker import LiveImage
+from databroker.databroker import DataBroker as db
+from skbeam.core.accumulators.binned_statistic import BinnedStatistic1D
+from streams.core import Stream
+# from functools import partial
+from xpdan.tools import better_mask_img
+
+import shed.event_streams as es
+from shed.event_streams import dstar
+
+
+def subs(img1, img2):
+    return img1 - img2
+
+
+def add(img1, img2):
+    return img1 + img2
+
+
+def pull_array(img2):
+    return img2
+
+
+def generate_binner(geo, mask):
+    img_shape = mask.shape
+    r = geo.rArray(img_shape)
+    q = geo.qArray(img_shape) / 10
+    q_dq = geo.deltaQ(img_shape) / 10
+
+    pixel_size = [getattr(geo, a) for a in ['pixel1', 'pixel2']]
+    rres = np.hypot(*pixel_size)
+    rbins = np.arange(np.min(r) - rres / 2., np.max(r) + rres / 2., rres / 2.)
+    rbinned = BinnedStatistic1D(r.ravel(), statistic=np.max, bins=rbins, )
+
+    qbin_sizes = rbinned(q_dq.ravel())
+    qbin_sizes = np.nan_to_num(qbin_sizes)
+    qbin = np.cumsum(qbin_sizes)
+    return BinnedStatistic1D(q.flatten, bins=qbin, mask=mask.flatten())
+
+
+def z_score_image(img, binner):
+    xy = binner.xy
+    for i in np.unique(xy):
+        tv = (xy == i)
+        img[tv] -= np.mean(img[tv])
+        img[tv] /= np.std(img[tv])
+    return img
+
+
+def integrate(img, binner):
+    return binner(img)
+
+
+def polarization_correction(img, geo, polarization_factor=.99):
+    return img / geo.polarization(img.shape, polarization_factor)
+
+
+def div(img, count):
+    return img / count
+
+
+def query_dark(db, docs):
+    doc = docs[0]
+    return db(uid=doc['sc_dk_field_uid'])
+
+
+def query_background(db, docs):
+    doc = docs[0]
+    return db(sample_name='kapton_film_bkgd',
+              is_dark={'$exists': False})
+
+
+def temporal_prox(res, docs):
+    doc = docs[0]
+    t = doc['time']
+    dt_sq = [(t - r['time']) ** 2 for r in res]
+    i = dt_sq.index(min(dt_sq))
+    return next(islice(dt_sq, i, i + 1))
+
+
+def load_geo(cal_params):
+    from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
+    ai = AzimuthalIntegrator()
+    ai.setPyFAI(**cal_params)
+    return ai
+
+
+def event_count(x):
+    return x['count'] + 1
+
+
+source = Stream(name='Foreground')
+source.sink(LiveImage('pe1_iamge'))
+
+fg_dark_stream = es.QueryUnpacker(db, es.Query(db, source,
+                                               query_function=query_dark,
+                                               query_decider=temporal_prox,
+                                               name='Query for FG Dark'))
+
+bg_query_stream = es.Query(db, source,
+                           query_function=query_background,
+                           name='Query for Background')
+
+bg_stream = es.QueryUnpacker(db, bg_query_stream)
+bg_dark_stream = es.QueryUnpacker(db, es.Query(db, bg_stream,
+                                               query_function=query_dark,
+                                               query_decider=temporal_prox,
+                                               name='Query for BG Dark'))
+
+# Perform dark subtraction on everything
+dark_sub_bg = es.map(dstar(subs),
+                     es.zip(bg_stream, bg_dark_stream),
+                     input_info={'img1': 'pe1_image',
+                                 'img2': 'pe1_image'},
+                     output_info=[('img', {'dtype': 'array',
+                                           'source': 'testing'})])
+
+# bundle the backgrounds into one stream
+bg_bundle = es.BundleSingleStream(dark_sub_bg, bg_query_stream,
+                                  name='Background Bundle')
+
+# sum the backgrounds
+summed_bg = es.accumulate(dstar(add), bg_bundle, start=dstar(pull_array),
+                          state_key='img1',
+                          input_info={'img2': 'img'},
+                          output_info=[('img', {
+                              'dtype': 'array',
+                              'source': 'testing'})])
+
+
+count_bg = es.accumulate(event_count, bg_bundle, start=1,
+                         state_key='count',
+                         output_info=[('count', {
+                             'dtype': 'int',
+                             'source': 'testing'})])
+
+ave_bg = es.map(dstar(div), es.zip(summed_bg, count_bg),
+                input_info={'img': 'img', 'count': 'count'},
+                output_info=[('img', {
+                    'dtype': 'array',
+                    'source': 'testing'})], name='Average Background')
+ave_bg.sink(pprint)
+
+dark_sub_fg = es.map(dstar(subs),
+                     es.zip(source,
+                            fg_dark_stream),
+                     input_info={'img1': 'pe1_image',
+                                 'img2': 'pe1_image'},
+                     output_info=[('img', {'dtype': 'array',
+                                           'source': 'testing'})],
+                     name='Dark Subtracted Foreground')
+
+dark_sub_fg.sink(LiveImage('img'))
+
+# combine the fg with the summed_bg
+fg_bg = es.combine_latest(dark_sub_fg, ave_bg, emit_on=dark_sub_fg)
+
+# subtract the background images
+fg_sub_bg = es.map(dstar(subs),
+                   fg_bg,
+                   input_info={'img1': 'img',
+                               'img2': 'img'},
+                   output_info=[('img', {'dtype': 'array',
+                                         'source': 'testing'})],
+                   name='Background Corrected Foreground')
+
+fg_sub_bg.sink(LiveImage('img'))
+
+# make/get calibration stream
+cal_md_stream = es.Eventify(source, start_key='calibration_md',
+                            output_info=[('calibration_md',
+                                          {'dtype': 'dict',
+                                           'source': 'workflow'})],
+                            md=dict(name='Calibration'))
+
+cal_stream = es.map(load_geo, cal_md_stream,
+                    input_info={'cal_params': 'calibration_md'},
+                    output_info=[('geo',
+                                  {'dtype': 'object', 'source': 'workflow'})])
+
+# polarization correction
+pfactor = .87
+p_corrected_stream = es.map(polarization_correction,
+                            es.zip(fg_sub_bg, cal_stream),
+                            input_info={'img': 'img',
+                                        'geo': 'geo'},
+                            output_info=[('img', {'dtype': 'array',
+                                                  'source': 'testing'})],
+                            polarization_factor=pfactor)
+
+# generate masks
+mask_kwargs = {}
+mask_stream = es.map(dstar(better_mask_img),
+                     es.combine_latest(p_corrected_stream, cal_stream,
+                                       emit_on=fg_sub_bg),
+                     input_info={'img': 'img',
+                                 'geo': 'geo'},
+                     output_info=[('mask', {'dtype': 'array',
+                                            'source': 'testing'})],
+                     **mask_kwargs)
+
+# generate binner stream
+binner_stream = es.map(dstar(generate_binner),
+                       es.combine_latest(mask_stream, cal_stream,
+                                         emit_on=mask_stream),
+                       input_info={'mask': 'mask',
+                                   'geo': 'geo'},
+                       output_info=[('binner', {'dtype': 'function',
+                                                'source': 'testing'})])
+
+# z-score the data
+z_score_stream = es.map(dstar(z_score_image),
+                        es.zip(p_corrected_stream, binner_stream),
+                        input_info={'img': 'img',
+                                    'binner': 'binner'},
+                        output_info=[('z_score_img', {'dtype': 'array',
+                                                      'source': 'testing'})])
+
+z_score_stream.sink(LiveImage('z_score_img'))
+
+iq_stream = es.map(dstar(integrate),
+                   es.zip(p_corrected_stream, binner_stream),
+                   input_info={'img': 'img',
+                               'binner': 'binner'},
+                   output_info=[('iq', {'dtype': 'array',
+                                        'source': 'testing'})])
