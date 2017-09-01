c1643d3e82040289b0377e2c28ac7d19db74eaba
Julien Lhermitte <jrmlhermitte@gmail.com>
2017-09-01 18:49:20 -0400

    add md feature to event stream simulation

--- shed/utils.py
+++ shed/utils.py
@@ -1,7 +1,7 @@
 from .event_streams import EventStream
 
 
-def to_event_model(data, output_info):
+def to_event_model(data, output_info, md):
     """Take an iterable of data and put it into the event model
 
     Parameters
@@ -10,6 +10,8 @@ def to_event_model(data, output_info):
         The data to be inserted
     output_info: list of tuple
         The name of the data and information to put into the descriptor
+    md : iterable of dicts
+        an iterable of dictionaries to use as metadata for the start documents
 
 
     Yields
@@ -24,6 +26,9 @@ def to_event_model(data, output_info):
     This is only for demonstration/example use, do not use for production.
     """
     es = EventStream(md={'source': 'to_event_model'}, output_info=output_info)
+    # add some metadata
+    es.md = md
+
     yield es.dispatch(('start', None))
     yield es.dispatch(('descriptor', None))
     for d in data:
