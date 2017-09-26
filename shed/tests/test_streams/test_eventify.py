5de0197cd49942be86d4e5c40dae601702acf5ee
christopher <cjwright4242@gmail.com>
2017-09-26 12:34:02 -0400

    FIX: fix the eventify_all test

--- shed/tests/test_streams/test_eventify.py
+++ shed/tests/test_streams/test_eventify.py
@@ -7,12 +7,14 @@ from shed.event_streams import star
 from shed.tests.utils import SinkAssertion
 from shed.utils import to_event_model
 
+# Normal behavior
 s1 = list(to_event_model(
     [np.random.random((10, 10)) for _ in range(5)],
     output_info=[('pe1_image', {'dtype': 'array'})],
     md={'name': 'test'}
 ))
 
+# No events, we should emit anyway
 s2 = list(to_event_model([], output_info=[('pe1_image', {'dtype': 'array'})],
                          md={'name': 'test'}))
 
@@ -61,11 +63,6 @@ def test_eventify_double(s):
     dp.sink(star(SinkAssertion(False)))
     dp.sink(print)
 
-    s = list(to_event_model(
-        [np.random.random((10, 10)) for _ in range(5)],
-        output_info=[('pe1_image', {'dtype': 'array'})],
-        md={'name': 'test'}
-    ))
     for _ in range(2):
         L.clear()
         for a in s:
@@ -96,11 +93,6 @@ def test_eventify_all(s):
     dp.sink(star(SinkAssertion(False)))
     dp.sink(print)
 
-    s = list(to_event_model(
-        [np.random.random((10, 10)) for _ in range(5)],
-        output_info=[('pe1_image', {'dtype': 'array'})],
-        md={'name': 'test'}
-    ))
     for _ in range(2):
         L.clear()
         for a in s:
@@ -109,7 +101,7 @@ def test_eventify_all(s):
         assert len(L) == 4
         assert_docs = set()
         # zip them since we know they're same length and order
-        for l, ll in zip(L, s):
+        for l in L:
             assert_docs.add(l[0])
             if l[0] == 'descriptor':
                 assert l[1]['data_keys']['name'] == {}
@@ -135,10 +127,6 @@ def test_eventify_descriptor_single(s):
     L = dp.sink_to_list()
     dp.sink(star(SinkAssertion(False)))
 
-    s = list(to_event_model(
-        [np.random.random((10, 10)) for _ in range(2)],
-        output_info=[('pe1_image', {'dtype': 'array'})]
-    ))
     for _ in range(2):
         L.clear()
         dk = None
@@ -173,10 +161,6 @@ def test_eventify_descriptor_double(s):
     L = dp.sink_to_list()
     dp.sink(star(SinkAssertion(False)))
 
-    s = list(to_event_model(
-        [np.random.random((10, 10)) for _ in range(2)],
-        output_info=[('pe1_image', {'dtype': 'array'})]
-    ))
     for _ in range(2):
         L.clear()
         dk = None
@@ -207,10 +191,6 @@ def test_eventify_descriptor_all(s):
     L = dp.sink_to_list()
     dp.sink(star(SinkAssertion(False)))
 
-    s = list(to_event_model(
-        [np.random.random((10, 10)) for _ in range(2)],
-        output_info=[('pe1_image', {'dtype': 'array'})]
-    ))
     for _ in range(2):
         L.clear()
         dk = None
@@ -232,31 +212,3 @@ def test_eventify_descriptor_all(s):
             if l[0] == 'stop':
                 assert l[1]['exit_status'] == 'success'
         assert set(assert_docs) == {'start', 'descriptor', 'event', 'stop'}
-
-
-@pytest.mark.parametrize('s', [s1, s2])
-def test_eventify_no_event(s):
-    source = Stream()
-
-    dp = es.Eventify(source, 'source',
-                     output_info=[('name', {
-                         'dtype': 'str',
-                         'source': 'testing'})])
-    L = dp.sink_to_list()
-    dp.sink(star(SinkAssertion(False)))
-    dp.sink(print)
-
-    for a in to_event_model([], output_info=[('img', {})]):
-        source.emit(a)
-
-    assert len(L) == 4
-    assert_docs = set()
-    # zip them since we know they're same length and order
-    for l in L:
-        assert_docs.add(l[0])
-        if l[0] == 'event':
-            assert l[1]['data']['name'] == 'to_event_model'
-        if l[0] == 'stop':
-            assert l[1]['exit_status'] == 'success'
-    for n in ['start', 'descriptor', 'event', 'stop']:
-        assert n in assert_docs
