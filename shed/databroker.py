7f0f629daf337dae781fc017a62fb283e24f9e29
christopher <cjwright4242@gmail.com>
2017-12-04 15:11:38 -0500

    WIP: add node for storing data in AssetStore. Saving to the databroker can be handled by `map(db.insert)`

---
+++ shed/databroker.py
@@ -0,0 +1,65 @@
+"""Nodes for adding data to the databroker"""
+from streamz import Stream
+
+
+class AssetInsert(Stream):
+    def __init__(self, upstream, fs, root,
+                 stream_name=None, external_writers=None):
+        super().__init__(upstream=upstream, stream_name=stream_name)
+        self.fs = fs
+        if external_writers is None:
+            external_writers = {}
+        self.external_writers = external_writers
+        self.writers = None
+
+    def update(self, x, who=None):
+        name, doc = x
+        fs_doc = getattr(self, name)(doc)
+        fs_doc.pop('filled', None)
+        fs_doc.pop('_name', None)
+        self._emit(fs_doc)
+
+    def start(self, doc):
+        # Make a fresh instance of any WriterClass classes.
+        self.writers = {data_key: cl(self.fs)
+                        for data_key, cl in self.external_writers.items()}
+        return 'start', doc, doc
+
+    def descriptor(self, doc):
+        fs_doc = dict(doc)
+        fs_doc['data_keys'] = dict(doc['data_keys'])
+        # Mutate fs_doc here to mark data as external.
+        for data_name in self.external_writers.keys():
+            # data doesn't have to exist
+            if data_name in fs_doc['data_keys']:
+                fs_doc['data_keys'][data_name] = dict(doc['data_keys'][data_name])
+                fs_doc['data_keys'][data_name].update(
+                    external='FILESTORE:')
+        return 'descriptor', doc, fs_doc
+
+    def event(self, doc):
+        fs_doc = dict(doc)
+        # We need a selectively deeper copy since we will mutate
+        # fs_doc['data'].
+        fs_doc['data'] = dict(fs_doc['data'])
+        # The writer writes data to an external file, creates a
+        # datum record in the filestore database, and return that
+        # datum_id. We modify fs_doc in place, replacing the data
+        # values with that datum_id.
+        for data_key, writer in self.writers.items():
+            # data doesn't have to exist
+            if data_key in fs_doc['data']:
+                fs_uid = writer.write(fs_doc['data'][data_key])
+                fs_doc['data'][data_key] = fs_uid
+
+        fs_doc.update(
+            filled={k: False for k in self.external_writers.keys()})
+        doc.update(filled={k: True for k in self.external_writers.keys()})
+        return 'event', doc, fs_doc
+
+    def stop(self, doc):
+        for data_key, writer in list(self.writers.items()):
+            writer.close()
+            self.writers.pop(data_key)
+        return 'stop', doc, doc
+
