8930d738fa35931d3daada9e8cdbc3ca8fa43a0b
christopher <cjwright4242@gmail.com>
2017-12-15 15:49:52 -0500

    ENH: comment example and show multiple graphs

--- examples/average.py
+++ examples/average.py
@@ -4,19 +4,39 @@ from bluesky.plans import count
 from shed.translation import FromEventStream, ToEventStream
 from streamz import Stream
 from bluesky.callbacks import LivePlot
+import matplotlib.pyplot as plt
 
-lp = LivePlot('average')
+# Create callbacks for plotting
+fig, ax = plt.subplots()
+lp = LivePlot('average', ax=ax)
+lp2 = LivePlot('binned', ax=ax)
+# lp3 = LivePlot('noisy_det', ax=ax)
+
 # Create a graph
 source = Stream()
+# Convert from raw event model to data
 fes = FromEventStream(source, 'event', ('data', 'noisy_det'), principle=True)
+
+# Averageing graph
 adder = fes.accumulate(lambda x, y: x + y)
 counter = fes.accumulate(lambda s, x: s + 1, start=0)
 averager = adder.zip(counter).map(lambda x: x[0]/x[1])
-tes = ToEventStream(averager, ('average',))
-tes.sink(print)
-tes.sink(lambda x: lp(*x))
 
+# Binned averaging
+sw = fes.sliding_window(2).map(sum).map(lambda x: x/2)
+
+# Convert back to Event Model
+tes1 = ToEventStream(averager, ('average',))
+tes2 = ToEventStream(sw, ('binned', ))
+
+# sink to plotting
+tes1.sink(lambda x: lp(*x))
+tes2.sink(lambda x: lp2(*x))
+
+# Run the scan
 RE = RunEngine()
 t = RE.subscribe(lambda *x: source.emit(x))
+# RE.subscribe(lp3)
 # RE.subscribe(print)
 RE(count([hw().noisy_det], 100))
+plt.show()
