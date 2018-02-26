6e9668bfb963f9701f9aaa869ff4366e4b4bf92d
christopher <cjwright4242@gmail.com>
2018-02-26 10:38:48 -0500

    bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shed',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     # description='data processing module',
     zip_safe=False,
