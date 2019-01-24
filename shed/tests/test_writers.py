dd5ec77837b7c726497e6fc23865ad6028f2fd5f
Christopher J. Wright <cjwright4242@gmail.com>
2019-01-23 20:24:10 -0500

    added test for double insertion

--- shed/tests/test_writers.py
+++ shed/tests/test_writers.py
@@ -14,7 +14,6 @@ from xpdan.vend.callbacks.core import Retrieve
 def test_storage(RE, hw, db, tmpdir):
     source = Stream()
     z = source.Store(str(tmpdir), NpyWriter)
-    lz = z.sink_to_list()
     z.starsink(db.insert)
 
     L = []
@@ -29,3 +28,29 @@ def test_storage(RE, hw, db, tmpdir):
         if n2 == 'event':
             print(d2['data']['img'])
             assert d2['data']['img'].shape == (10, 10)
+
+
+def test_double_storage(RE, hw, db, tmpdir):
+    source = Stream()
+    z = source.Store(str(tmpdir), NpyWriter)
+    z.starsink(db.insert)
+
+    L = []
+    RE.subscribe(lambda *x: L.append(x))
+    RE(bp.count([hw.direct_img], 2))
+
+    LL = []
+    RE.subscribe(lambda *x: LL.append(x))
+    RE(bp.count([hw.direct_img], 2))
+    for nd1, nd2 in zip(L, LL):
+        source.emit(nd1)
+        source.emit(nd2)
+
+    rt = Retrieve(handler_reg=db.reg.handler_reg)
+    for ii in [-2, -1]:
+        for i, nd in enumerate(db[ii].documents()):
+            n2, d2 = rt(*nd)
+            print(n2)
+            if n2 == 'event':
+                print(d2['data']['img'])
+                assert d2['data']['img'].shape == (10, 10)
--- shed/writers.py
+++ shed/writers.py
@@ -1,3 +1,4 @@
+from bluesky.utils import sanitize_np
 from rapidz import Stream
 import os
 import numpy as np
@@ -11,10 +12,11 @@ class Store(Stream):
         if writer is None:
             writer = {}
         self.writer = writer
-        self.init_writer = None
         self.root = root
         self.resource_kwargs = resource_kwargs
+        self.init_writers = {}
         self.descriptors = {}
+        self.not_issued_descriptors = set()
 
     def update(self, x, who=None):
         name, doc = x
@@ -23,19 +25,21 @@ class Store(Stream):
         doc = dict(doc)
 
         if name == "start":
-            self.init_writer = self.writer(
+            self.init_writers[doc['uid']] = self.writer(
                 self.root, doc, self.resource_kwargs
             )
-            self.descriptors = {}
         if name == 'descriptor':
             self.descriptors[doc['uid']] = doc
+            self.not_issued_descriptors.add(doc['uid'])
             return
 
         elif name == "event":
             ret = []
-            for n, d in self.init_writer.write(doc):
+            writer = self.init_writers[self.descriptors[doc['descriptor']]['run_start']]
+            for n, d in writer.write(doc):
                 # If this is an event and we haven't done this descriptor yet
-                if n == 'event' and doc['descriptor'] in self.descriptors:
+                if n == 'event' and doc['descriptor'] in self.not_issued_descriptors:
+
                     # For each of the filled keys let us know that it is backed
                     # by FILESTORE
                     descriptor = self.descriptors[doc['descriptor']]
@@ -44,11 +48,14 @@ class Store(Stream):
                             descriptor['data_keys'][k].update(
                                 external='FILESTORE:')
                     ret.append(self.emit(('descriptor', descriptor)))
-                    # We're done with that descriptor now
-                    self.descriptors.pop(doc['descriptor'])
-                ret.append(self.emit((n, d)))
 
-            return [self.emit(out) for out in self.init_writer.write(doc)]
+                    # We're done with that descriptor now
+                    self.not_issued_descriptors.remove(doc['descriptor'])
+                ret.append(self.emit((n, d)))
+            return ret
+        elif name == 'stop':
+            # clean up our cache
+            self.init_writers.pop(doc['run_start'])
 
         return self.emit((name, doc))
 
@@ -66,7 +73,7 @@ class NpyWriter:
 
     def write(self, event):
         for k, v in event["data"].items():
-            if isinstance(v, np.ndarray):
+            if isinstance(v, np.ndarray) and v.shape != ():
                 resource_path = f'an_data/{event["uid"]}_{k}.npy'
                 fpath = os.path.join(self.root, resource_path)
                 os.makedirs(os.path.dirname(fpath), exist_ok=True)
@@ -83,4 +90,7 @@ class NpyWriter:
                 yield "datum", datum
                 event['data'][k] = datum["datum_id"]
                 event['filled'][k] = False
+            # Don't write a file just for a single number!
+            elif isinstance(v, np.ndarray) and v.shape == ():
+                event['data'][k] = sanitize_np(v)
         yield "event", event
