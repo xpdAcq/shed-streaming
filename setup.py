3b20cdd0966225c4acfe5eb1c6f3f7ef2ba481b0
Christopher J. Wright <cjwright4242@gmail.com>
2019-02-03 19:28:27 -0500

    bumped version to 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shed',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     # description='data processing module',
     zip_safe=False,
