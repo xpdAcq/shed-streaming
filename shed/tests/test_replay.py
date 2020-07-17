09abc4edf1ed234145c3dd74f59a54858fe0b742
st3107 <st3107@columbia.edu>
2020-07-17 13:27:11 -0400

    BUG: fix test_replay

--- shed/tests/test_replay.py
+++ shed/tests/test_replay.py
@@ -35,7 +35,7 @@ def test_replay(db):
     for yy in y(5):
         l0.append(yy)
         db.insert(*yy)
-        yield g1.update(yy)
+        g1.update(yy)
 
     assert "env" in db[-1].start
     print("start replay")
@@ -90,7 +90,7 @@ def test_replay_dummy_node(db):
     for yy in y(5):
         l0.append(yy)
         db.insert(*yy)
-        yield source.emit(yy)
+        source.emit(yy)
 
     print("start replay")
 
@@ -141,9 +141,9 @@ def test_replay_parallel(db):
     for yy in y(5):
         l0.append(yy)
         db.insert(*yy)
-        yield g1.update(yy)
+        g1.update(yy)
     while len(l1) < len(l0):
-        yield gen.sleep(.01)
+        gen.sleep(.01)
 
     print("start replay")
 
@@ -163,7 +163,7 @@ def test_replay_parallel(db):
         parents[v["node"]].update(data[v["uid"]])
 
     while len(l2) < len(l0):
-        yield gen.sleep(.01)
+        gen.sleep(.01)
 
     # check that all the things are ok
     assert len(l1) == len(l2)
@@ -204,7 +204,7 @@ def test_replay_numpy(db):
     for yy in y(5):
         l0.append(yy)
         db.insert(*yy)
-        yield g1.update(yy)
+        g1.update(yy)
 
     print("start replay")
 
@@ -264,10 +264,10 @@ def test_replay_parallel_numpy(db):
     for yy in y(5):
         l0.append(yy)
         db.insert(*yy)
-        yield g11.update(yy)
-        yield g1.update(yy)
+        g11.update(yy)
+        g1.update(yy)
     while len(l1) < len(l0):
-        yield gen.sleep(.01)
+        gen.sleep(.01)
 
     print("start replay")
 
@@ -290,7 +290,7 @@ def test_replay_parallel_numpy(db):
         parents[v["node"]].update(data[v["uid"]])
 
     while len(l2) < len(l0):
-        yield gen.sleep(.01)
+        gen.sleep(.01)
 
     # check that all the things are ok
     assert len(l1) == len(l2)
@@ -305,36 +305,3 @@ def test_replay_parallel_numpy(db):
         assert nd1[0] == nd2[0]
         if nd1[0] == "event":
             assert nd1[1]["data"]["img2"] == nd2[1]["data"]["img2"]
-
-
-# def test_replay_export(db):
-#     print("build graph")
-#     g1 = FromEventStream(
-#         "event", ("data", "det_image"), principle=True, stream_name="g1"
-#     )
-#     g11 = FromEventStream("event", ("data", "det_image"), stream_name="g11")
-#     g11_1 = g1.zip(g11)
-#     g2 = g11_1.starmap(op.mul).map(np.log)
-#     g = g2.ToEventStream(("img2",))
-#     L = g.sink_to_list()
-#     dbf = g.DBFriendly()
-#     dbf.sink(print)
-#     dbf.starsink(db.insert)
-#
-#     print("run experiment")
-#     for yy in y():
-#         db.insert(*yy)
-#         g11.update(yy)
-#         g1.update(yy)
-#
-#     print(L[0][1]["uid"])
-#     s1 = db[L[0][1]["uid"]]["stop"]
-#     print("replay experiment")
-#     rp = replay(db, db[L[0][1]["uid"]], export=True)
-#     next(rp)
-#     next(rp)
-#     assert s1 != db[-1]["stop"]
-#     for (_, a), (_, b) in zip(db[-2].documents(), db[-1].documents()):
-#         assert a["uid"] != b["uid"]
-#         if "data" in a:
-#             assert a["data"] == b["data"]
