6552d5bb5ed924f48a90ec7c5abd87f877703871
st3107 <st3107@columbia.edu>
2020-07-21 16:01:38 -0400

    bumped version to 0.7.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shed',
-    version='0.7.3',
+    version='0.7.4',
     packages=find_packages(),
     # description='data processing module',
     zip_safe=False,
